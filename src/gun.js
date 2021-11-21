2fbba4f1e23e50178136452c99b199c7166fd56a
Ronald Prato <ronald.prato@unet.edu.ve>
2021-11-20 21:47:51 -0500

    logic for relay peer connection changed

--- src/gun.js
+++ src/gun.js
@@ -2,11 +2,7 @@ import Gun from "gun/gun"
 import "gun/sea"
 import "gun/axe"
 
-const relayPeer =
-  process.env.ENV === "DEVELOP"
-    ? "http://localhost:3030/gun"
-    : process.env.RELAY_SERVER_URL
-
+const relayPeer = process.env.RELAY_SERVER_URL
 const gun = Gun([relayPeer])
 const user = gun.user().recall({ sessionStorage: true })
 
